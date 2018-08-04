017f1ca5b7547e77cfadef5927b8b6c62b92cee7
jennifer kerssen <jjkerssen@gmail.com>
2018-08-04 09:12:50 -0600

    split client and server

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 'use strict'; 
 
+exports.SERVER_URL = process.env.SERVER_URL || 'http://localhost:8080';
 exports.DATABASE_URL = process.env.MONGODB_URI || 'mongodb://localhost/network';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/network-test';
 exports.PORT = process.env.PORT || 8080;
--- websites/index.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-const { Website } = require('./models');
-const { router } = require('./router');
-
-module.exports = { Website, router };
