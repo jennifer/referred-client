ff05360431e1cc697e86bf00ddd08d0b820fd2f2
jennifer kerssen <jjkerssen@gmail.com>
2018-09-03 09:52:01 -0600

    add redux dev tools

--- src/store.js
+++ src/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { reducer as formReducer } from 'redux-form';
 import thunk from 'redux-thunk';
 import { loadAuthToken } from './local-storage';
@@ -7,20 +8,21 @@ import networkReducer from './reducers/network-reducer';
 import { setAuthToken, refreshAuthToken } from './actions/auth';
 
 const store = createStore(
-    combineReducers({
-        form: formReducer,
-        auth: authReducer,
-        network: networkReducer
-    }),
-    window.REDUX_DEVTOOLS_EXTENSION && window.REDUX_DEVTOOLS_EXTENSION(),
-    applyMiddleware(thunk)
+  combineReducers({
+    form: formReducer,
+    auth: authReducer,
+    network: networkReducer
+  }),
+  composeWithDevTools(
+  	applyMiddleware(thunk)
+  )
 );
 
 const authToken = loadAuthToken();
 if (authToken) {
-    const token = authToken;
-    store.dispatch(setAuthToken(token));
-    store.dispatch(refreshAuthToken());
+  const token = authToken;
+  store.dispatch(setAuthToken(token));
+  store.dispatch(refreshAuthToken());
 }
 
 export default store;
