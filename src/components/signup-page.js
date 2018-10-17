f5f64906757d79292d96f060d6c93f2a4a39dab2
jennifer kerssen <jjkerssen@gmail.com>
2018-10-16 21:19:26 -0400

    fix signup redirect

--- src/components/signup-page.js
+++ src/components/signup-page.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import About from './about';
-import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Link, Redirect } from 'react-router-dom';
 
 import SignupForm from './signup-form';
 
-export default function SignupPage(props) {
-  
+export function SignupPage(props) {
+  if (props.loggedIn) {
+    return <Redirect to='/dashboard' />;
+  }
   return (
     <React.Fragment>
+      <About />
       <div className='block login-float border-left login-width'>
-        <h1 className='margin-bottom'>Make an Account</h1>
+        <h1 className='margin-bottom'>Make an account:</h1>
         <SignupForm />
         <p>or</p>
         <Link to='/' className='italic underline highlight'>Go back to login</Link>
       </div>
-      <About />
     </React.Fragment>
   );
 }
+
+const mapStateToProps = state => ({
+  loggedIn: state.auth.currentUser !== null
+});
+
+export default connect(mapStateToProps)(SignupPage);
