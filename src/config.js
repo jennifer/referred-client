d5aaabc5aec55f27da40ab3a78d9d0c62d9f0ca5
jennifer kerssen <jjkerssen@gmail.com>
2018-10-18 18:35:24 -0600

    update config route

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const API_BASE_URL =
   window.location.href.indexOf('localhost') > -1
     ? 'http://localhost:8080/api'
-    : 'https://secret-inlet-35294.herokuapp.com';
+    : 'https://secret-inlet-35294.herokuapp.com/api';
